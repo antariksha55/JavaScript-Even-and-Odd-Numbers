cea0acb50ef1a5aa6a78ad61d43ac7d0e3fcfff0
antariksha55 <antarikshasamanta55@gmail.com>
2026-09-18 18:28:59 +0530

    Add even-odd number checker

---
+++ even-odd.js
@@ -0,0 +1,9 @@
+let number = prompt("Enter a number:");
+
+number = Number(number);
+
+if (number % 2 === 0) {
+    alert(number + " is an Even Number");
+} else {
+    alert(number + " is an Odd Number");
+}
